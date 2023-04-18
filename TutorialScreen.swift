3dd10d033eeafb78792d4ad89df66afe1cecb111
hajime <impapa68@gmail.com>
2023-04-19 05:38:44 +0900

    add : Tutorial Screen

---
+++ TutorialScreen.swift
@@ -0,0 +1,20 @@
+//
+//  TutorialScreen.swift
+//  Sound & Sight Explorer
+//
+//  Created by Ha Jong Myeong on 2023/04/19.
+//
+
+import SwiftUI
+
+struct TutorialScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TutorialScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialScreen()
+    }
+}
