ca70f1e3f3d67b4997072ede93a0e564dc67a9f1
hajime <impapa68@gmail.com>
2023-04-19 05:38:56 +0900

    fix : landingView

--- LandingView.swift
+++ LandingView.swift
@@ -16,8 +16,8 @@ struct LandingView: View {
                     .font(.largeTitle)
                     .bold()
                     .foregroundColor(Color.white)
-            .padding()
-            Spacer()
+                    .padding()
+                Spacer()
                 NavigationLink(destination: SoundGameScreen(composition: Composition(notes: defaultComposition))
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Image("002").resizable().ignoresSafeArea())
@@ -25,20 +25,27 @@ struct LandingView: View {
                     Text("Sound Explorer")
                         .font(.title3)
                         .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 96/255, green: 252/255, blue: 255/255, opacity: 1), color2: Color(Color.RGBColorSpace.sRGB, red: 44/255, green: 158/255, blue: 238/255, opacity: 1), color3: Color(Color.RGBColorSpace.sRGB, red: 0/255, green: 129/255, blue: 255/255, opacity: 1))
-                
-                .padding()
+                    
+                        .padding()
+                }
+                NavigationLink(destination: SightGameScreen()) {
+                    Text("Sight Explorer")
+                        .font(.title3)
+                        .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 96/255, green: 252/255, blue: 255/255, opacity: 1), color2: Color(Color.RGBColorSpace.sRGB, red: 44/255, green: 158/255, blue: 238/255, opacity: 1), color3: Color(Color.RGBColorSpace.sRGB, red: 0/255, green: 129/255, blue: 255/255, opacity: 1))
+                    
+                        .padding()
+                }
+                Spacer()
+                NavigationLink(destination: TutorialScreen()){
+                    Text("Tutorial")
+                        .font(.title2)
+                        .foregroundColor(.yellow)
+                        .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 255/255, blue: 102/255, opacity: 0.7), color2: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 255/255, blue: 0/255, opacity: 0.5), color3: Color(Color.RGBColorSpace.sRGB, red: 204/255, green: 204/255, blue: 0/255, opacity: 0.3))
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("001").resizable().ignoresSafeArea())
         }
-        NavigationLink(destination: SightGameScreen()) {
-            Text("Sight Explorer")
-                .font(.title3)
-                .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 96/255, green: 252/255, blue: 255/255, opacity: 1), color2: Color(Color.RGBColorSpace.sRGB, red: 44/255, green: 158/255, blue: 238/255, opacity: 1), color3: Color(Color.RGBColorSpace.sRGB, red: 0/255, green: 129/255, blue: 255/255, opacity: 1))
-        
-        .padding()
-    }
-    Spacer()
-}
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(Image("001").resizable().ignoresSafeArea())
-}
-    .navigationViewStyle(.stack)}
+        .navigationViewStyle(.stack)}
 }
