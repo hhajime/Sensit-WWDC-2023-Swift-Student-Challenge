dc421e72e9a27a432a89a68c662c5aaf07b7e6eb
hajime <impapa68@gmail.com>
2023-04-20 08:26:00 +0900

    delete : unused code

--- Butterflies/ButterfliesScreen.swift
+++ Butterflies/ButterfliesScreen.swift
@@ -8,22 +8,15 @@
 import SwiftUI
 
 struct ButterfliesScreen: View {
-    @State private var scrollProgress: CGFloat = 0
     @StateObject private var audioPlayer = AudioPlayer()
     private let soundFile = "002"
-    let backgroundImage = Image("003")
     var body: some View {
         ZStack{
             Butterflies()
-        } .edgesIgnoringSafeArea(.all)
-            .onAppear {
-                withAnimation(Animation.linear(duration: 300)) {
-                    scrollProgress = 35
-                }
-            }
-            .background(.black)
-            .onAppear{audioPlayer.play(soundFile)}
-            .onDisappear{audioPlayer.stop()}
+                .background(.black)
+        }
+        .onAppear{audioPlayer.play(soundFile)}
+        .onDisappear{audioPlayer.stop()}
     }
     
 }
